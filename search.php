660b46178915ca69f95b291013ee324b53501a72
Jason Kayser <jpk2f2@mail.missouri.edu>
2017-12-05 14:45:34 -0600

    Most of the stuff I've worked on

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+$link = mysqli_connect("127.0.0.1", "jkayser","","meme_site");
+
+if(!$link){
+	echo "Error: unable to connect to MySQL";
+	exit;
+}
+
+$stmt = mysqli_prepare($link, "SELECT id FROM Pictures WHERE title=? ");
+mysqli_stmt_bind_param($stmt, 's', $searchString);
+
+$searchString=$_POST['search_string'];
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_bind_result($stmt, $id);
+
+mysqli_stmt_fetch($stmt);
+
+mysqli_stmt_close($stmt);
+
+
+
+
+mysqli_close($link);
+?>
